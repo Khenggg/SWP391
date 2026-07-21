25433cb22d12e2dc68386d1ee6ad02a2fbfca212
NguyenHung <nguyenhung22424@gmail.com>
2026-07-22 06:18:34 +0700

    Revert "fix(frontend): remove Content-Type application/json when posting FormData in coreAxiosClient to allow browser to send multipart/form-data boundary"
    This reverts commit 3bbbfaacb4a8ba130934e693414d793ce46e7130.

--- frontend/src/api/coreAxiosClient.js
+++ frontend/src/api/coreAxiosClient.js
@@ -18,9 +18,6 @@ coreAxiosClient.interceptors.request.use(
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
-    if (config.data instanceof FormData) {
-      delete config.headers["Content-Type"];
-    }
     return config;
   },
   (error) => Promise.reject(error)
